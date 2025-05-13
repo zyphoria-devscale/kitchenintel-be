4351b7cf6e34127a253af9a27415a7af9c2bbd47
Amir <mfathulqorib97@gmail.com>
2025-05-13 09:47:55 +0700

    add query params

--- menu/views.py
+++ menu/views.py
@@ -8,6 +8,12 @@ class MenuCreateView(generics.ListCreateAPIView):
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
 
+    def get_queryset(self):
+        category_id = self.request.query_params.get("category_id")
+        if category_id:
+            return Menu.objects.filter(category_id=category_id)
+        return Menu.objects.all()
+
 class MenuDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
