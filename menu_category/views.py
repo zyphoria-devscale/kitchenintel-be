caf1242fcf3d2ef3daf05ccf4fc3b8d3106a25e8
Amir <mfathulqorib97@gmail.com>
2025-05-13 09:27:16 +0700

    add query parameter

--- menu_category/views.py
+++ menu_category/views.py
@@ -8,6 +8,13 @@ class MenuCategoryCreateView(generics.ListCreateAPIView):
     queryset = MenuCategory.objects.all()
     serializer_class = MenuCategorySerializer
 
+    def get_queryset(self):
+        queryset = MenuCategory.objects.all()
+        parent_id = self.request.query_params.get('parent_id', None)
+        if parent_id is not None:
+            queryset = queryset.filter(parent_id=parent_id)
+        return queryset
+
     def get_serializer_context(self):
         context = super().get_serializer_context()
         # Add any additional context if needed
