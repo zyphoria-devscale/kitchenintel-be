467449fa1e9f3bbc47959e80ad9067217bc1631d
Muhammad Ilham Hidayat <m.ilham.hidayat.95@gmail.com>
2025-05-18 09:29:48 +0700

    feat: authentication and authorization using token

--- core/settings.py
+++ core/settings.py
@@ -31,6 +31,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
     'menu_category',
     'menu',
     'order',
@@ -62,6 +63,15 @@ TEMPLATES = [
         },
     },
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ]
+}
 
 WSGI_APPLICATION = 'core.wsgi.application'
 
--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import include, path
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("menu_category.urls")),
     path("api/", include("menu.urls")),
     path("api/", include("order.urls")),
+    path("api/login", obtain_auth_token, name="api_token_auth")
 ]
