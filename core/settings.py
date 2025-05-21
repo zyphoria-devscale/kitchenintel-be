39e2f348c735018cf627af277827f652ac19df0d
Muhammad Ilham Hidayat <m.ilham.hidayat.95@gmail.com>
2025-05-21 15:58:26 +0700

    feat: add order pagination (#12)

--- core/settings.py
+++ core/settings.py
@@ -105,6 +105,11 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
